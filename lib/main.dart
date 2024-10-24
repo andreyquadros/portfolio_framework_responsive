f5cae52d31d771966d53bb7dd5a22459721dd895
andreyquadros <andrey.quadros@ifro.edu.br>
2024-10-24 16:19:21 -0400

    1.1 - responsividade básica

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:responsive_web/view/home_page.dart';
 
 void main(){
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
+    builder: (context, child) => ResponsiveBreakpoints.builder(
+      child: child!,
+      breakpoints: [
+        const Breakpoint(start: 0, end: 450, name: MOBILE),
+        const Breakpoint(start: 451, end: 800, name: TABLET),
+        const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+        const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+      ],
+    ),
     home: HomePage(),
   ));
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,35 +19,45 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(color: Color(0xFF808080), fontSize: 16),
         ),
         backgroundColor: Color(0xFF232323),
-      actions: [
-        ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(Color(0xFF232323)),
-            shape: WidgetStateProperty.all(
-              RoundedRectangleBorder(
-                side: BorderSide(
-                  color: Color(0xFF808080), // Cor da borda
-                  width: 1,), // Espessura da borda,
-                borderRadius: BorderRadius.circular(
-                    7), // Defina o valor desejado aqui
+        actions: [
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(Color(0xFF232323)),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Color(0xFF808080), // Cor da borda
+                    width: 1,
+                  ), // Espessura da borda,
+                  borderRadius:
+                      BorderRadius.circular(7), // Defina o valor desejado aqui
+                ),
               ),
             ),
+            onPressed: () {},
+            child: Text(
+              'Contato',
+              style: TextStyle(color: Color(0xFF808080), fontSize: 16),
+            ),
           ),
-          onPressed: () {},
-          child: Text(
-            'Contato',
-            style: TextStyle(color: Color(0xFF808080), fontSize: 16),
-          ),
-        ),
-        SizedBox(width: 24,)
-      ],
+          SizedBox(
+            width: 24,
+          )
+        ],
       ),
       backgroundColor: Color(0xFF232323),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset('images/header.png')
+            ResponsiveVisibility(
+                visible: false,
+                visibleConditions: [Condition.largerThan(name: MOBILE)],
+                child: Image.asset('images/header.png')),
+            ResponsiveVisibility(
+                visible: false,
+                visibleConditions: [Condition.equals(name: MOBILE)],
+                child: Text('VISÍVEL APENAS NO MOBILE',style: TextStyle(color: Colors.white),)),
           ],
         ),
       ),
